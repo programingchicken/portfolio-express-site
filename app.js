a8a818ee4b473326ef17ec768881f451c70a96d6
programingchicken <aquacrazz.gp@gmail.com>
2021-10-14 00:36:41 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const index = require('./public/js/index')
+const about = require('./public/js/about')
+
+app.set('view engine', 'pug')
+
+
+
+app.use('/static', express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extend: false }));
+
+app.use('/projects', index)
+app.use('/about', about)
+
+
+
+app.listen(3000);
---
+++ public/js/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const data = require('../data/data.json').data
+const { projects } = data 
+
+console.log(projects.length)
+
+
+router.get('/:id' , (req, res, next) => {
+    const { id } = req.params
+    const {title, image, liveLink, description, technologies, githubLink} = projects[id - 1]
+     res.render('project' , { title , image, liveLink, description, technologies, githubLink })
+})
+
+router.get('/' , (req, res, next) => {
+    const projectsArray = []
+    console.log(projectsArray)
+    // const { id } = req.params
+    projects.forEach(project => {
+        projectsArray.push(project)
+
+    })
+    console.log(projectsArray)
+    res.render('index', {projectsArray} )
+
+})
+
+module.exports = router;
