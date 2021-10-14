f1e96e9ac25995926f738e68c9c89fc3ea0f1379
programingchicken <aquacrazz.gp@gmail.com>
2021-10-14 02:06:22 -0700

    3rd commit

--- src/api.js
+++ src/api.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const index = require('./public/js/index')
-const about = require('./public/js/about')
+const index = require('./public/js/routs')
+const about = require('../public/js/about')
+const serverless = require('serverless-http');
+
+const router = express.Router()
+
 
 app.set('view engine', 'pug')
 
-
+router.get(index)
 
 app.use('/static', express.static('public'))
 app.use(bodyParser.json())
@@ -18,3 +22,7 @@ app.use('/about', about)
 
 // app.use('/portfolio-express-site', router)
 app.listen(80)
+
+app.use('/.netlify/functions/app', router);
+
+module.exports.handler = serverless(app)
