f91b811d38d21b6303e5a0dd40593f75a9dbf6ed
programingchicken <aquacrazz.gp@gmail.com>
2021-10-14 20:10:01 -0700

    5th commit

--- public/js/routes.js
+++ public/js/routes.js
@@ -4,18 +4,25 @@ const data = require('../data/data.json').data
 const { projects } = data 
 
 
-// console.log(projects.length)
+console.log(projects.length)
 
 
 router.get('/:id' , (req, res, next) => {
     const { id } = req.params
     const projectBuild = projects[id - 1]
-     res.render('project.pug' , {projectBuild})
+     res.render('project' , {projectBuild})
 })
 
 router.get('/' , (req, res, next) => {
-    console.log(projects)
-    res.render('./index.pug', projects )
+    const projectsArray = []
+    console.log(projectsArray)
+    // const { id } = req.params
+    projects.forEach(project => {
+        projectsArray.push(project)
+
+    })
+    console.log(projectsArray)
+    res.render('index', {projectsArray} )
 
 })
 
