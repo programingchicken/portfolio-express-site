7b38335d3f205f1b03229d9b49874739766580e0
programingchicken <aquacrazz.gp@gmail.com>
2022-02-19 22:09:44 -0800

    fixed names

--- public/js/routes.js
+++ public/js/routes.js
@@ -9,26 +9,20 @@ console.log(projects.length)
 //get one of the project pages
 router.get('/project/:id', (req, res, next) => {
     const { id } = req.params
-    const { title, description, liveLink, githubLink, image, technologies } = projects[id - 1]
-    res.render('project', { title, description, liveLink, githubLink, image, technologies })
+    const { project_name, description, live_link, github_link, image_urls, technologies } = projects[id - 1]
+    res.render('project', { project_name, description, live_link, github_link, image_urls, technologies })
+
 })
 
 //get about page
 router.get('/about', (req, res) => {
     res.render('about')
+
 })
 
 router.get('/', (req, res, next) => {
+res.redirect('project')
 
-    const projectsArray = []
-    console.log(projectsArray)
-    // const { id } = req.params
-    projects.forEach(project => {
-        projectsArray.push(project)
-
-    })
-    console.log(projectsArray)
-    res.render('index', { projectsArray })
 })
 
 //get projects page 
@@ -43,6 +37,7 @@ router.get('/project', (req, res, next) => {
     })
     console.log(projectsArray)
     res.render('index', { projectsArray })
+
 })
 
 module.exports = router;
